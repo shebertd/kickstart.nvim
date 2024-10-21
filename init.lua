c728439bfa732b2613c770d37d13141351b0234f
Simon Hébert-Deschamps <7688355+shebertd@users.noreply.github.com>
2024-10-21 11:31:05 -0400

    add debugger

--- init.lua
+++ init.lua
@@ -861,6 +861,7 @@ require('lazy').setup({
     config = function()
       require('monokai-pro').setup {
         dim_inactive_windows = true,
+        devicons = true,
       }
     end,
     init = function()
@@ -951,7 +952,7 @@ require('lazy').setup({
   --  Here are some example plugins that I've included in the Kickstart repository.
   --  Uncomment any of the lines below to enable them (you will need to restart nvim).
   --
-  -- require 'kickstart.plugins.debug',
+  require 'kickstart.plugins.debug',
   require 'kickstart.plugins.indent_line',
   require 'kickstart.plugins.lint',
   -- require 'kickstart.plugins.autopairs',
